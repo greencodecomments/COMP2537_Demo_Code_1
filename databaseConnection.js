78639c1b1e8a17482f1e98a723bf3fd743a028b3
GreenCodeComments <pguichon1@bcit.ca>
2024-06-27 12:59:18 -0700

    Removing deprecated mongodb connection parameters

--- databaseConnection.js
+++ databaseConnection.js
@@ -6,5 +6,5 @@ const mongodb_password = process.env.MONGODB_PASSWORD;
 
 const MongoClient = require("mongodb").MongoClient;
 const atlasURI = `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/?retryWrites=true`;
-var database = new MongoClient(atlasURI, {useNewUrlParser: true, useUnifiedTopology: true});
+var database = new MongoClient(atlasURI, {});
 module.exports = {database};
